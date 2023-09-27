0a4ce21a0e6f3251a4fc44c6cd55a8d1fdc21dda
dinhdatle <111744077+dinhdatle@users.noreply.github.com>
2023-09-27 15:42:27 +0700

    fix

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,27 +1,28 @@
 // import argv from 'minimist'
 // const options = argv(process.argv.slice(2))
+
 import {config} from 'dotenv'
 import fs from 'fs'
 import path from 'path'
 const env = process.env.NODE_ENV
-const evnFilename = `.env.${env}`
-if(!env){
-  console.log('Chua cung cap bien moi truong NODE_ENV (vi du: development,production')
-  console.log(`Phat hien NODE_ENV= ${env}`)
-  process.exit(1)
-}
-console.log(`Phat hien NODE_ENV= ${env}, vi the app se dung file moi truong la ${evnFilename}`)
-if(!fs.existsSync(path.resolve(evnFilename))){
-  console.log(`khong tin thay file mot truong ${evnFilename} `)
-  console.log(`Luu y : app khong dung fi;e .env, vi du moi truong development thi app se dung file .env.development`)
-  console.log(`Vui long tao file ${evnFilename} va than khoi noi dung o file .env.example`)
-  process.exit(1)
+// const evnFilename = `.env.${env}`
+// if(!env){
+//   console.log('Chua cung cap bien moi truong NODE_ENV (vi du: development,production')
+//   console.log(`Phat hien NODE_ENV= ${env}`)
+//   process.exit(1)
+// }
+// console.log(`Phat hien NODE_ENV= ${env}, vi the app se dung file moi truong la ${evnFilename}`)
+// if(!fs.existsSync(path.resolve(evnFilename))){
+//   console.log(`khong tin thay file mot truong ${evnFilename} `)
+//   console.log(`Luu y : app khong dung fi;e .env, vi du moi truong development thi app se dung file .env.development`)
+//   console.log(`Vui long tao file ${evnFilename} va than khoi noi dung o file .env.example`)
+//   process.exit(1)
 
-}
+// }
 
-config({
-  path: '.env'
-})
+// config({
+//   path: '.env'
+// })
 
 
 
